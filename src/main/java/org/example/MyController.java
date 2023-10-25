7ad726a2063a99c895fa626b410a2301165aac45
grifnmi <mgriffin37@universityofgalway.ie>
2023-10-25 19:42:44 +0100

    test

---
+++ src/main/java/org/example/MyController.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from spring boot";
+    }
+}
